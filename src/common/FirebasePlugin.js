55face18e91975cc918732b608d418a1f27019c9
Misaka <ppq1991@gmail.com>
2016-05-09 18:38:11 +0800

    update

--- src/common/FirebasePlugin.js
+++
@@ -1,9 +0,0 @@
-import Firebase from 'firebase'
-
-const url = 'http://eleme-flowchart.firebaseio.com'
-
-export default {
-  install (Vue) {
-    Vue.prototype.$firebase = new Firebase(url)
-  }
-}
